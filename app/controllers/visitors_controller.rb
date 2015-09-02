ef020685cc27444624b1e7121b86d3c72dfe27b9
Dan Solo <dan@solocreative.com.au>
2015-09-03 08:51:10 +1000

    Add simple layouts and image for header logo

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,6 @@
 class VisitorsController < ApplicationController
   def new
     @owner = Owner.new
+    #render 'visitors/new', :layout => false ...  to restrict the layout/application file from loading set to false. If you want to use an alternative layout name it instead of false eg 'special'
   end
 end
