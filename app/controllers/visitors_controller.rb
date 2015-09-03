f683b8694b5a484d8bbbc605476933c1b0d291cf
Dan Solo <dan@solocreative.com.au>
2015-09-04 08:55:52 +1000

    Add Zurb Foundation and add my own awesome retro color palette

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,7 +2,7 @@ class VisitorsController < ApplicationController
   def new
     @owner = Owner.new
     #render 'visitors/new', :layout => false ...  to restrict the layout/application file from loading set to false. If you want to use an alternative layout name it instead of false eg 'special'
-    flash.now[:notice] = 'Welcome!'
-    flash[:alert] = 'My birthday is soon.'
+    #flash.now[:notice] = 'Welcome!'
+    #flash[:alert] = 'My birthday is soon.'
   end
 end
